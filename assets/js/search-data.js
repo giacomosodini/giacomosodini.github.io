9130a7c7a5d2be33f05aa03b4e175a7b69515fab
giacomosodini <36991421+giacomosodini@users.noreply.github.com>
2025-03-16 17:26:14 +0000

    Deploying to gh-pages from @ giacomosodini/giacomosodini.github.io@e4a4a3f9599988ce6a7eb4cc31b719fa84a3f785 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -448,34 +448,27 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%67%69%61%63%6F%6D%6F.%73%6F%64%69%6E%69@%75%6E%69%76%69%65.%61%63.%61%74", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-orcid',
+        title: 'ORCID',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://orcid.org/0000-0001-9806-3576", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
+        id: 'social-researchgate',
+        title: 'ResearchGate',
         section: 'Socials',
         handler: () => {
-          window.open("/feed.xml", "_blank");
+          window.open("https://www.researchgate.net/profile/Giacomo-Sodini/", "_blank");
         },
       },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=TxwgVk0AAAAJ", "_blank");
         },
       },];
