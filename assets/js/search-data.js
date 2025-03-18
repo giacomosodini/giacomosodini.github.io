97ea07c25cdfbd54f98d94a65a9dff6c9c3d0aeb
giacomosodini <36991421+giacomosodini@users.noreply.github.com>
2025-03-18 07:35:52 +0000

    Deploying to gh-pages from @ giacomosodini/giacomosodini.github.io_new@a2777144d08d1d8142b2ca753c77a25f12c24669 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -458,18 +458,18 @@ ninja.data = [{
           window.open("https://scholar.google.com/citations?user=TxwgVk0AAAAJ", "_blank");
         },
       },{
-        id: 'social-orcid',
-        title: 'ORCID',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://orcid.org/0000-0001-9806-3576", "_blank");
-        },
-      },{
         id: 'social-arxiv',
         title: 'arXiv',
         section: 'Socials',
         handler: () => {
           window.open("https://osf.arxiv.com/a/sodini_g_1/", "_blank");
+        },
+      },{
+        id: 'social-orcid',
+        title: 'ORCID',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://orcid.org/0000-0001-9806-3576", "_blank");
         },
       },{
         id: 'social-researchgate',
