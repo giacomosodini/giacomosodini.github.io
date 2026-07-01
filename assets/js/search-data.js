e2a107300244e52a57ab30f1caf779c5b8509479
giacomosodini <36991421+giacomosodini@users.noreply.github.com>
2026-07-01 14:12:21 +0000

    Deploying to gh-pages from @ giacomosodini/giacomosodini.github.io@caa15863dae02c5f05aaeefc7382176fc587c410 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,6 +37,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/teaching/";
           },
+        },{id: "nav-service",
+          title: "Service",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/service/";
+          },
         },{id: "post-a-post-with-image-galleries",
       
         title: "a post with image galleries",
